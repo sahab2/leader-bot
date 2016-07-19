a078985f911e8719e1075b81c4a22565fe72a4ae
sahab2 <sahab.beidokhti@gmail.com>
2016-07-19 22:36:35 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {193350313},--Sudo users
+    sudo_users = {265230332},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
